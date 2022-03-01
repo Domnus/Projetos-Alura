415d7729190eb8604c1ff69196e64e0ac2a78ab7
Bento Carlos <bcsilva49@gmail.com>
2022-03-01 14:01:21 -0300

    03 - Retornando dados

--- C#/dotnet/FilmesAPI/Controllers/FilmeController.cs
+++ C#/dotnet/FilmesAPI/Controllers/FilmeController.cs
@@ -12,11 +12,32 @@ namespace FilmesAPI.Controller
     public class FilmeController : ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
 
         [HttpPost]
-        public void AdicionaFilme([FromBody] Filme filme)
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
+            filme.Id = id++;
             filmes.Add(filme);
+
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new {Id = filme.Id}, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmesPorId(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if (filme != null)
+                return Ok(filme);
+            else
+                return NotFound();
         }
     }
 }
--- C#/dotnet/FilmesAPI/Models/Filme.cs
+++ C#/dotnet/FilmesAPI/Models/Filme.cs
@@ -8,11 +8,16 @@ namespace FilmesAPI.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
+
         [Required(ErrorMessage = "O campo título é obrigatório")]
         public string Titulo { get; set; }
+
         [Required(ErrorMessage = "O campo diretor é obrigatório")]
         public string Diretor { get; set; }
+
         public string Genero { get; set; }
+
         [Range(1, 600, ErrorMessage = "A duração deve ter entre 1 e 600 minutos")]
         public int Duracao { get; set; }
     }
